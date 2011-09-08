a92de0583aad27f06708cb948a5edf65feb4e73c
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2011-09-08 13:21:47 +0100

    move to a forked group-id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject drift "1.2.2"
+(defproject org.clojars.mccraigmccraig/drift "1.2.3-SNAPSHOT"
   :description "Drift is a rails like migration framework for Clojure."
   :dependencies [[clojure-tools "1.0.0"]
                  [org.clojure/clojure "1.2.0"]
